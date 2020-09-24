db43f0c4df2e607f1d78b44631d4ef1337615829
Dmitry Kushnir <sendmail.dima@gmai.com>
2020-09-24 10:48:12 +0300

    HW-shop-cart

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ICartProduct} from '../../../../../shared/mocks/1-components/cart-product';
 
 @Component({
   selector: 'app-shop-card',
   templateUrl: './shop-card.component.html',
 })
 
+
 export class ShopCardComponent {
+  @Input() product: ICartProduct = {} as ICartProduct;
+  @Output() increment = new EventEmitter<number>();
+  @Output() decrement = new EventEmitter<number>();
+
+  public decrementProductInCart() {
+    this.decrement.emit(this.product?.count);
+  }
+
+  public incrementProductInCart() {
+    this.increment.emit(this.product?.count);
+  }
 
 }
