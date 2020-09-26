a08332a9da135d24fe6b33d911f3ee91e2068cb3
Dmitry Kushnir <sendmail.dima@gmai.com>
2020-09-26 22:11:38 +0300

    HW header

--- 1-components/5-header/src/app/header/header.component.ts
+++ 1-components/5-header/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-header',
@@ -6,5 +6,11 @@ import { Component, ChangeDetectionStrategy} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+  @Output() goToBasket  = new EventEmitter();  // @Output()  addToCart = new EventEmitter();
+  // public goToProduct: EventEmitter<void> = new EventEmitter<void>();
+public counter = 5;
 
+public redirectTo(){
+this.goToBasket.emit();
+}
 }
